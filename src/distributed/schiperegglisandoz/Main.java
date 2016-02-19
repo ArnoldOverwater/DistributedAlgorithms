19812a5dce8ff8a873fd73936c79609632188b7f
Arnold Overwater <arnoldoverwater@gmail.com>
2016-02-19 15:12:23 +0100

    Made the process truly distributed

--- src/distributed/schiperegglisandoz/Main.java
+++ src/distributed/schiperegglisandoz/Main.java
@@ -1,28 +1,47 @@
 package distributed.schiperegglisandoz;
 
-import java.net.MalformedURLException;
+import java.io.IOException;
 import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
 
 public class Main {
 
-	public static void main(String[] args) throws RemoteException, MalformedURLException, AlreadyBoundException, NotBoundException {
-		LocateRegistry.createRegistry(1099);
-		int n = 3;
-		Process[] processes = new Process[n];
-		SESInterface[] remoteRefs = new SESInterface[n];
-		for (int i = 0; i < n; i++) {
-			processes[i] = new Process(i, n);
-			String url = "rmi://localhost:1099/process"+i;
-			Naming.bind(url, processes[i]);
-			remoteRefs[i] = (SESInterface) Naming.lookup(url);
+	//	public static void main(String[] args) throws RemoteException, MalformedURLException, AlreadyBoundException, NotBoundException {
+	//		LocateRegistry.createRegistry(1099);
+	//		int n = 3;
+	//		Process[] processes = new Process[n];
+	//		SESInterface[] remoteRefs = new SESInterface[n];
+	//		for (int i = 0; i < n; i++) {
+	//			processes[i] = new Process(i, n);
+	//			String url = "rmi://localhost:1099/process"+i;
+	//			Naming.bind(url, processes[i]);
+	//			remoteRefs[i] = (SESInterface) Naming.lookup(url);
+	//		}
+	//		for (int i = 0; i < n; i++)
+	//			for (int j = 0; j < n; j++)
+	//				processes[i].setProcess(j, remoteRefs[j]);
+	//	}
+
+	public static void main(String[] args) throws AlreadyBoundException, NotBoundException, IOException {
+		if (args.length <= 2) {
+			System.err.println("Usage: <Process-ID> <binding-URL> <lookup-URLs>...");
+			System.exit(-1);
 		}
+
+		int id = Integer.parseInt(args[0]);
+		int n = args.length - 2;
+		System.out.println(n+" processes");
+		Process process = new Process(id, n);
+		Naming.bind(args[1], process);
+		System.out.println("Process with id "+id+" bound at "+args[1]);
+
+		System.out.print("Press key when all processes are bound");
+		System.in.read();
+
 		for (int i = 0; i < n; i++)
-			for (int j = 0; j < n; j++)
-				processes[i].setProcess(j, remoteRefs[j]);
+			process.setProcess(i, (SESInterface) Naming.lookup(args[i + 2]));
+		System.out.println("Network registered successfully");
 	}
 
 }
