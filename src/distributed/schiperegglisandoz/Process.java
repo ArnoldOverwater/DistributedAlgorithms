7dc21517e2c1cf1bb703e9866025e4ac9722a6ef
Arnold Overwater <arnoldoverwater@gmail.com>
2016-02-20 13:44:04 +0100

    Refactored Process with many bug fixes

--- src/distributed/schiperegglisandoz/Process.java
+++ src/distributed/schiperegglisandoz/Process.java
@@ -9,37 +9,75 @@ import java.util.List;
 public class Process extends UnicastRemoteObject implements SESInterface {
 	private static final long serialVersionUID = -6056565009547143029L;
 
-//	private int numberNodes;
 	private int myId;
 	private int[] clock;
 	private int[][] buffer;
 	private SESInterface[] processes;
 	private List<Message> messageBuffer;
-	private List<String> messages;
+
+	protected List<Message> sent;
+	protected List<Message> delivered;
 
 	public Process(int id, int n) throws RemoteException {
 		super(0);
-//		this.numberNodes = n;
 		this.myId = id;
 		this.clock = new int[n];
 		this.buffer = new int[n][];
-//		this.buffer[id] = new int[n];
 		this.processes = new SESInterface[n];
 		this.messageBuffer = new ArrayList<Message>();
-		this.messages = new ArrayList<String>();
+		this.sent = new ArrayList<Message>();
+		this.delivered = new ArrayList<Message>();
 	}
 
-	public synchronized void send(String text, int recipient) throws RemoteException {
-		clock[myId]++;
-		Message m = new Message(text, myId, buffer, clock);
-		buffer[recipient] = clock.clone();
+	public void send(String text, int recipient) throws RemoteException {
+		Message m;
+		synchronized (this) {
+			clock[myId]++;
+			m = new Message(text, myId, deepClone(buffer), clock.clone());
+			buffer[recipient] = clock.clone();
+			sent.add(m);
+			System.out.println("Sent "+m);
+		}
+		processes[recipient].receive(m);
+	}
+
+	public void send(String text, int recipient, long delay) throws RemoteException {
+		Message m;
+		synchronized (this) {
+			clock[myId]++;
+			m = new Message(text, myId, deepClone(buffer), clock.clone());
+			buffer[recipient] = clock.clone();
+			sent.add(m);
+			System.out.println("Sent "+m);
+		}
+		try {
+			Thread.sleep(delay);
+		} catch (InterruptedException e) {
+		}
 		processes[recipient].receive(m);
 	}
 
 	@Override
-	public synchronized void receive(Message m) {
-		messageBuffer.add(m);
-		checkDeliveries();
+	public void receive(Message m) {
+		synchronized (messageBuffer) {
+			messageBuffer.add(m);
+			checkDeliveries();
+		}
+	}
+
+	protected void deliver(Message m) {
+		synchronized (this) {
+			clock[myId]++;
+			clock = vMax(clock, m.getTimestamp());
+			for (int i = 0; i < buffer.length; i++) {
+				int[] iBuffer = m.getBuffer(i);
+				if (buffer[i] == null)
+					buffer[i] = iBuffer;
+				else if (iBuffer != null)
+					buffer[i] = vMax(buffer[i], iBuffer);
+			}
+			System.out.println("Delivered "+m);
+		}
 	}
 
 	public int getMyId() {
@@ -60,25 +98,14 @@ public class Process extends UnicastRemoteObject implements SESInterface {
 			Message m = iterator.next();
 			int[] mBuffer = m.getBuffer(myId);
 			if (mBuffer == null || vLtEq(mBuffer, clock)) {
-				//deliver message
-				messages.add(m.getMessage());
-				clock[myId]++;
+				deliver(m);
 				//remove message from messageBuffer
 				iterator.remove();
-				//Update vector clock
-				for (int i = 0; i < buffer.length; i++) {
-					int[] iBuffer = m.getBuffer(i);
-					if (buffer[i] == null)
-						buffer[i] = iBuffer;
-					else if (iBuffer != null)
-						buffer[i] = vMax(buffer[i], iBuffer);
-				}
 				//reset the iterator so we can check all messages again
 				iterator = messageBuffer.iterator();
 			}
 		}
 	}
-
 
 	private int[] vMax(int[] vector1, int[] vector2) {
 		int[] res = new int[vector1.length];
@@ -95,4 +122,12 @@ public class Process extends UnicastRemoteObject implements SESInterface {
 		return true;
 	}
 
+	private int[][] deepClone(int[][] original) {
+		int[][] result = new int[original.length][];
+		for (int i = 0; i < original.length; i++) {
+			if (original[i] != null)
+				result[i] = original[i].clone();
+		}
+		return result;
+	}
 }
