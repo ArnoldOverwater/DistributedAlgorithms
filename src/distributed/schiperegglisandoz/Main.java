1863bd2956519829f5467a7a9e154647fa33a9e8
Arnold Overwater <arnoldoverwater@gmail.com>
2016-03-02 16:46:31 +0100

    Revamped the Main method

--- src/distributed/schiperegglisandoz/Main.java
+++ src/distributed/schiperegglisandoz/Main.java
@@ -4,26 +4,11 @@ import java.io.IOException;
 import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
+import java.util.Random;
 
 public class Main {
 
-	//	public static void main(String[] args) throws RemoteException, MalformedURLException, AlreadyBoundException, NotBoundException {
-	//		LocateRegistry.createRegistry(1099);
-	//		int n = 3;
-	//		Process[] processes = new Process[n];
-	//		SESInterface[] remoteRefs = new SESInterface[n];
-	//		for (int i = 0; i < n; i++) {
-	//			processes[i] = new Process(i, n);
-	//			String url = "rmi://localhost:1099/process"+i;
-	//			Naming.bind(url, processes[i]);
-	//			remoteRefs[i] = (SESInterface) Naming.lookup(url);
-	//		}
-	//		for (int i = 0; i < n; i++)
-	//			for (int j = 0; j < n; j++)
-	//				processes[i].setProcess(j, remoteRefs[j]);
-	//	}
-
-	public static void main(String[] args) throws AlreadyBoundException, NotBoundException, IOException {
+	public static void main(String[] args) throws AlreadyBoundException, NotBoundException, IOException, InterruptedException {
 		if (args.length <= 2) {
 			System.err.println("Usage: <Process-ID> <binding-URL> <lookup-URLs>...");
 			System.exit(-1);
@@ -36,12 +21,24 @@ public class Main {
 		Naming.bind(args[1], process);
 		System.out.println("Process with id "+id+" bound at "+args[1]);
 
-		System.out.print("Press key when all processes are bound");
-		System.in.read();
+		System.out.print("Waiting 10 seconds before other processed are bound");
+		Thread.sleep(10000L);
 
 		for (int i = 0; i < n; i++)
 			process.setProcess(i, (SESInterface) Naming.lookup(args[i + 2]));
 		System.out.println("Network registered successfully");
+		
+		Random rand = new Random();
+		int m = 5 + rand.nextInt(5);
+		for (int i = 0; i < m; i++) {
+			// Random idle time
+			Thread.sleep(rand.nextInt(5) * 100L);
+			int recipient = rand.nextInt(n - 1);
+			if (recipient == id)
+				recipient++;
+			// Send with random delay
+			process.send(id+"-"+i, recipient, rand.nextInt(5) * 1000L);
+		}
 	}
 
 }
