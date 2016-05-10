ff13478677d2fccea230b5f4309f9076037e7d28
Arnold Overwater <arnoldoverwater@gmail.com>
2016-05-10 16:35:59 +0200

    Added the missing ampersant to the  command

--- src/distributed/mst/test/sh/Common.java
+++ src/distributed/mst/test/sh/Common.java
@@ -7,7 +7,7 @@ import distributed.mst.Edge;
 public class Common {
 
 	public static void generateSHFile(PrintStream file, Edge[][] edges, boolean[] startProcesses) {
-		file.println("rmiregistry");
+		file.println("rmiregistry &");
 		for (int i = 0; i < startProcesses.length; i++) {
 			if (startProcesses[i])
 				file.print("java distributed.mst.test.MainStart");
