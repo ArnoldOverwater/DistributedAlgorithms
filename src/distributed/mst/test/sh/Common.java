91f17160d41c5cbed8d60abfa3920bd2d373de55
Arnold Overwater <arnoldoverwater@gmail.com>
2016-05-10 17:22:19 +0200

    Improvements in SH generation

--- src/distributed/mst/test/sh/Common.java
+++ src/distributed/mst/test/sh/Common.java
@@ -7,18 +7,19 @@ import distributed.mst.Edge;
 public class Common {
 
 	public static void generateSHFile(PrintStream file, Edge[][] edges, boolean[] startProcesses) {
+		int n = startProcesses.length;
 		file.println("rmiregistry &");
-		for (int i = 0; i < startProcesses.length; i++) {
+		for (int i = 0; i < n; i++) {
 			if (startProcesses[i])
 				file.print("java distributed.mst.test.MainStart");
 			else
 				file.print("java distributed.mst.test.Main");
-			file.print(" "+i+" rmi://localhost:1099/");
+			file.print(" "+i+" "+n+" rmi://localhost:1099/");
 			for (int j = 0; j < edges[i].length; j++) {
 				if (edges[i][j] != null)
 					file.print(" "+j+" "+edges[i][j].weight);
 			}
-			file.println(" &");
+			file.println(" > mst_process"+i+".log &");
 		}
 	}
 
